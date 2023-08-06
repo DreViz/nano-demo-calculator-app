bc95c13646509a986339aca738ca4e161aae887a
vizdre <vaibhavdeori1@gmail.com>
2023-08-06 19:51:17 +0530

    done

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
+<<<<<<< HEAD
 from flask import Flask,jasonify,request
+=======
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
+>>>>>>> parent of 0084e0f (Remove actual answers)
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
+<<<<<<< HEAD
     return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
@@ -21,10 +32,21 @@ def add():
             return jsonify({"error": str(e)}), 400
 
     return jsonify({"error": "Invalid input"}), 400
+=======
+    return 'Hello world!'
+
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
+>>>>>>> parent of 0084e0f (Remove actual answers)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
