e49722cd74df66acac075ae756c8d2399a2dade9
vizdre <vaibhavdeori1@gmail.com>
2023-08-06 19:54:19 +0530

    done\

--- server.py
+++ server.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask,jasonify,request
-=======
 from flask import Flask, request, jsonify
 from dataclasses import dataclass
 
@@ -8,31 +5,12 @@ from dataclasses import dataclass
 class Result:
     result: int
 
->>>>>>> parent of 0084e0f (Remove actual answers)
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-<<<<<<< HEAD
-    return 'Hello World!'
-
-@app.route("/calculator/add", methods=['POST'])
-def add():
-    data = request.get_json()
-    if 'num1' in data and 'num2' in data:
-        num1 = data['num1']
-        num2 = data['num2']
-
-        try:
-            result = num1 + num2
-            return jsonify({"result": result})
-        except Exception as e:
-            return jsonify({"error": str(e)}), 400
-
-    return jsonify({"error": "Invalid input"}), 400
-=======
     return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
@@ -40,7 +18,6 @@ def add():
     numbers = request.json
     response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
->>>>>>> parent of 0084e0f (Remove actual answers)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
