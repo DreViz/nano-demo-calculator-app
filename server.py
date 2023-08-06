6fa5f083f873c63952450bb35b32a58f4fdc6bc7
vizdre <vaibhavdeori1@gmail.com>
2023-08-06 19:50:00 +0530

    done

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask,jasonify,request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    if 'num1' in data and 'num2' in data:
+        num1 = data['num1']
+        num2 = data['num2']
+
+        try:
+            result = num1 + num2
+            return jsonify({"result": result})
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
+
+    return jsonify({"error": "Invalid input"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
